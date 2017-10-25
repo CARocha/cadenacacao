a209b14921384f908171bdcdd0961c0987b392e8
Erick Murillo <erickmurillo22@gmail.com>
2017-10-25 15:28:39 -0600

    templates

--- directorio/views.py
+++ directorio/views.py
@@ -137,7 +137,7 @@ def user_profile(request,template='perfil_user.html'):
 		if form.is_valid():
 			mensaje = form.cleaned_data['mensaje']
 		try:
-			subject, from_email = 'Solicitud ingreso Cadena de cacao', 'noreply@unag-datos.org'
+			subject, from_email = 'Solicitud ingreso de organización o especialista a Sistema Cadena de cacao', 'noreply@unag-datos.org'
 			text_content = 'Usuario: ' + str(user.username) + '<br>'  + \
 							'Correo: ' + str(user.email) + '<br>'  + \
 							'Mensaje: ' + str(mensaje)
