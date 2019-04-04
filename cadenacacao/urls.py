b893e156899479bee4470bee54d078ddea50e18f
Carlos Rocha <crocha09.09@gmail.com>
2019-04-03 23:39:29 -0600

    agregando el template de login y autenticacion social

--- cadenacacao/urls.py
+++ cadenacacao/urls.py
@@ -35,7 +35,7 @@ urlpatterns = [
     # url(r'^accounts/logout/$', auth_views.logout,{'next_page': '/'}),
     url(r'^accounts/profile/$',user_profile,name='profile'),
     # url(r'^accounts/profile/editar/(?P<slug>[\w-]+)/$',editar_org,name='editar-org'),
-    # url(r'^accounts/profile/user/editar/$',editar_user,name='editar-user'),
+    url(r'^accounts/profile/user/editar/$',editar_user,name='editar-user'),
 
     url(r'^password_reset/$', auth_views.password_reset, name='password_reset'),
     url(r'^password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
